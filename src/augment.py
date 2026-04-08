03c3c1cb248446303a8e2b072305ffe91d2374aa
Jeet-solanki-1 <solankijeet285@gmail.com>
2026-04-08 10:07:48 +0530

    feat: augment_data.py implement with all neccesury funtions

--- src/augment.py
+++ src/augment.py
@@ -8,7 +8,7 @@ import random
 from scipy.ndimage import rotate, shift, zoom
 from PIL import Image
 
-def random_roation(image: np.ndarray, max_angle:float=1q5) -> np.ndarray:
+def random_rotation(image: np.ndarray, max_angle:float = 15) -> np.ndarray:
 	"""
 	Rotate image by random angle between -max_angle and +max_angel.
 
@@ -21,8 +21,8 @@ def random_roation(image: np.ndarray, max_angle:float=1q5) -> np.ndarray:
 	"""
 
 	angle  = random.uniform(-max_angle,+max_angle)
-	roatated = rotate(image,angle,reshape=False, order=1)
-	return np.clip(roatated,0,1)
+	rotated = rotate(image,angle,reshape=False, order=1)
+	return np.clip(rotated,0,1)
 
 def random_flip(image:np.ndarray) -> np.ndarray:
 	"""
@@ -58,6 +58,28 @@ def random_shift(image: np.ndarray, max_shift: float=0.1) -> np.ndarray:
 	shifted = shift(image, [shift_h, shift_w, 0], mode='nearest')
 	return shifted
 
+def random_zoom(image: np.ndarray, max_zoom: float=0.1) -> np.ndarray:
+	"""
+	Zoom in or out slightly.
+
+	Args:
+		image: Input image array (H,W,3)
+		max_zoom: Maximum zoom factor (0.1 = +-10%)
+	"""
+	h,w = image.shape[:2]
+	zoom_factor = 1+ random.uniform(-max_zoom, max_zoom)
+	zoomed = zoom(image, [zoom_factor,zoom_factor,1],order=1)
+
+	#Crop or pad to original size
+	if zoomed.shape[0]>h:
+		start = (zoomed.shape[0]-h)//2
+		zoomed = zoomed[start:start+h,start:start+w]
+	else:
+		pad_h = (h - zoomed.shape[0])//2
+		pad_w = (w - zoomed.shape[1])//2
+		zoomed = np.pad(zoomed, ((pad_h,pad_h),(pad_w,pad_w),(0,0)), mode='edge')
+	return np.clip(zoomed[:h,:w],0,1)
+
 def augment_image(image: np.ndarray, intensity:str = "medium") -> np.ndarray:
 	"""
 	Apply a random cmbination of augmentations.
@@ -83,7 +105,7 @@ def augment_image(image: np.ndarray, intensity:str = "medium") -> np.ndarray:
 
 	#Rotatio (always apply for medium/heavy)
 	if random.random()>0.3:
-		img = random_roation(img, p["angle"])
+		img = random_rotation(img, p["angle"])
 
 	#Flip (50% chance)
 	img = random_flip(img)
@@ -94,6 +116,11 @@ def augment_image(image: np.ndarray, intensity:str = "medium") -> np.ndarray:
 
 	#Shift 
 	if random.random()>0.5:
-		img = random_zoom(img, p["shift"])
+		img = random_shift(img, p["shift"])
+
+	#Zoom
+	if random.random() > 0.5:
+		img = random_zoom(img,p["zoom"])
+
 
 	return img
